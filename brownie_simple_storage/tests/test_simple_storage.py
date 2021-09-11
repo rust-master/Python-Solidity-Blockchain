0e1f2608190347dfbfe370b57d97237276e0e7c5
Muhammad Zaryab Rafique <muhammadzaryabrafique@gmail.com>
2021-09-11 13:31:18 +0500

    testing smart contract

---
+++ brownie_simple_storage/tests/test_simple_storage.py
@@ -0,0 +1,12 @@
+from brownie import SimpleStorage, accounts
+
+
+def test_deploy():
+    # Arrange
+    account = accounts[0]
+    # Act
+    simple_storage = SimpleStorage.deploy({"from": account})
+    starting_value = simple_storage.get()
+    expected = 0
+    # Assert
+    assert starting_value == expected
