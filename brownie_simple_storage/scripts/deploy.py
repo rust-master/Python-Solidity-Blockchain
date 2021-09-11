8e801ff3a3c2ad20580dd3ff6b6f4fd1e265800d
Muhammad Zaryab Rafique <muhammadzaryabrafique@gmail.com>
2021-09-11 13:07:32 +0500

    transaction and retrive

--- brownie_simple_storage/scripts/deploy.py
+++ brownie_simple_storage/scripts/deploy.py
@@ -6,6 +6,11 @@ def deploy_simple_storage():
     account = accounts[0]
     simple_storage = SimpleStorage.deploy({"from": account})
     print("SimpleStorage:", simple_storage)
+    store_value = simple_storage.get()
+    print("Value:", store_value)
+    transation = simple_storage.set(42, {"from": account})
+    transation.wait(1)
+    print("Updated Value:", simple_storage.get())
 
 
 def main():
