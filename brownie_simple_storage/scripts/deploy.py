cadb7edb1ae4a171333704ed61e6e207ff53054d
Muhammad Zaryab Rafique <muhammadzaryabrafique@gmail.com>
2021-09-11 15:19:20 +0500

    deploy to rinkeby testnet

--- brownie_simple_storage/scripts/deploy.py
+++ brownie_simple_storage/scripts/deploy.py
@@ -1,9 +1,9 @@
-from brownie import accounts, config, SimpleStorage
+from brownie import accounts, config, SimpleStorage, network
 import os
 
 
 def deploy_simple_storage():
-    account = accounts[0]
+    account = get_account()
     simple_storage = SimpleStorage.deploy({"from": account})
     print("SimpleStorage:", simple_storage)
     store_value = simple_storage.get()
@@ -11,6 +11,13 @@ def deploy_simple_storage():
     transation = simple_storage.set(42, {"from": account})
     transation.wait(1)
     print("Updated Value:", simple_storage.get())
+
+
+def get_account():
+    if network.show_active() == "development":
+        return accounts[0]
+    else:
+        return accounts.add(config["wallets"]["from_key"])
 
 
 def main():
