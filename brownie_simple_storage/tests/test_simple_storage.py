763b9d589b5b19867a92b79f8ba8284279c0821f
Muhammad Zaryab Rafique <muhammadzaryabrafique@gmail.com>
2021-09-11 13:38:13 +0500

    test

--- brownie_simple_storage/tests/test_simple_storage.py
+++ brownie_simple_storage/tests/test_simple_storage.py
@@ -10,3 +10,15 @@ def test_deploy():
     expected = 0
     # Assert
     assert starting_value == expected
+
+
+def test_updating_storage():
+    # Arrange
+    account = accounts[0]
+    # Act
+    simple_storage = SimpleStorage.deploy({"from": account})
+    expected = 5
+    simple_storage.set(expected, {"from": account})
+    updated_value = simple_storage.get()
+    # Assert
+    assert expected == updated_value
